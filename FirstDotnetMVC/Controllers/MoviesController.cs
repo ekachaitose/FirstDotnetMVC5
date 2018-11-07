280d71d9e8021444e65ded99bf35da186f375cfc
ekachaitose <ekachaitose11@gmail.com>
2018-11-07 10:58:59 +0700

    add project

---
+++ FirstDotnetMVC/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using FirstDotnetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstDotnetMVC.ViewModels;
+
+namespace FirstDotnetMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Index()
+        {
+            var movie = GetMovie();
+
+         
+            //var customer = new List<Customer>
+            //{
+            //    new Customer {Name = "Customer 1"},
+            //    new Customer {Name = "Customer 3"}
+            //};
+
+            //var viewModel = new RandomMovieViewModel
+            //{
+            //    Movie = movie,
+            //    Customers = customer
+            //};
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovie()
+        {
+            return new List<Movie>
+            {
+                new Movie {Name = "Fantastic Beats"},
+                new Movie {Name = "The Lord of The Ring"}
+            };
+        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("Id = "+id);
+        //}
+
+       
+
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)  } ")]
+        //public ActionResult ByReleaseYear(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+    }
+}
---
+++ FirstDotnetMVC/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FirstDotnetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstDotnetMVC.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+
+    }
+}
